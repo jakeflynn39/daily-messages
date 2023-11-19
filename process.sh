b9be0bf42324c8f1bd3e24b0d5c32d861bc622fd
JAke Flynn <jpflynn01@gmail.com>
2023-11-19 11:55:00 -0800

    updated process

--- process.sh
+++ process.sh
@@ -3,20 +3,27 @@
 source venv/bin/activate 
 source .env
 printf "Environment activated\n"
+
 pip install tweepy openai python-dotenv
 printf "Dependencies installed\n"
+
 pip3 freeze > requirements.txt
 pip3 install --target ./package -r requirements.txt
 printf "Dependencies packaged\n"
+
 cd package
 zip -r ../deployment-package.zip .
 printf "Package zipped\n"
+
 cd ..
 zip -g deployment-package.zip dailyAffirmations.py friends.json prev_tweets.json gptRole.txt
 printf "Files zipped\n"
 printf "Deployment package ready\n"
+
 aws s3 cp deployment-package.zip "s3://$S3_BUCKET_NAME/"
 printf "Package uploaded to S3\n"
+
 aws lambda update-function-code --function-name $FUNCTION_NAME --s3-bucket $S3_BUCKET_NAME --s3-key deployment-package.zip
 printf "Lambda function updated\n"
+
 deactivate
