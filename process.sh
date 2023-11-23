6690d4cd89b4a04cffd6198b95e347fe2f1b2e5b
JAke Flynn <jpflynn01@gmail.com>
2023-11-23 08:05:34 -0800

    added the dates folder in the zip command

--- process.sh
+++ process.sh
@@ -16,7 +16,7 @@ zip -r ../deployment-package.zip .
 printf "Package zipped\n"
 
 cd ..
-zip -g deployment-package.zip dailyAffirmations.py friends.json prev_tweets.json gptRole.txt
+zip -g deployment-package.zip dailyAffirmations.py friends.json prev_tweets.json gptRole.txt dates/*
 printf "Files zipped\n"
 printf "Deployment package ready\n"
 
