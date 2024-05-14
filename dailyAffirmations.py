13370522ab32a7fa1db09b1af2d31c18734387cc
JAke Flynn <jpflynn01@gmail.com>
2024-05-14 14:41:56 -0700

    changed gpt model

--- dailyAffirmations.py
+++ dailyAffirmations.py
@@ -98,7 +98,7 @@ def handler(event, context):
         messages.append({"role": "user", "content": tweet_directive + friend_info})
 
         response = openai.ChatCompletion.create(
-            model="gpt-4-turbo-preview",
+            model="gpt-4",
             messages=messages,
             temperature=1.09,
         )
